9b09c9e2e6eacc9ff3ec7d8f9dccf5e772cb240d
Maria Olanipekun <m.olanipekun@hotmail.com>
2020-08-28 02:38:19 +0100

    create question component

---
+++ src/components/Question.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Question = () => {
+  return (
+    <div
+      style={{
+        minHeight: "25vh",
+        width: "60vw",
+        margin: "auto",
+        padding: "1rem 1.2rem",
+        fontSize: "1.5rem",
+        fontWeight: "700",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      Is there anything you feel you have learned or changed your mind about as
+      a result of this crisis?
+    </div>
+  );
+};
+
+export default Question;
