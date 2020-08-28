baef4a3690f76cd30f9b3e18b989e1f802e10c2a
Maria Olanipekun <m.olanipekun@hotmail.com>
2020-08-28 13:40:02 +0100

    update font styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./assets/fonts/Avenir.ttc";
 import App from "./App";
 // import * as serviceWorker from './serviceWorker';
 
